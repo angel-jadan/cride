ac6a9cc492d9cffa446610b2a1049aa6848beaaa
Angel <angel_jadan@outlook.com>
2022-01-08 19:59:17 -0500

    list members

---
+++ cride/circles/views/circles.py
@@ -0,0 +1,53 @@
+"""Circle views."""
+
+# Django REST Framework
+from rest_framework import mixins, viewsets
+
+# Permissions
+from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
+
+# Serializers
+from cride.circles.serializers import CircleModelSerializer
+
+# Models
+from cride.circles.models import Circle, Membership
+
+
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
+    """Circle view set."""
+
+    serializer_class = CircleModelSerializer
+    lookup_field = 'slug_name'
+
+    def get_queryset(self):
+        """Restrict list to public-only."""
+        queryset = Circle.objects.all()
+        if self.action == 'list':
+            return queryset.filter(is_public=True)
+        return queryset
+
+    def get_permissions(self):
+        """Assign permissions based on action."""
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
+
+    def perform_create(self, serializer):
+        """Assign circle admin."""
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
