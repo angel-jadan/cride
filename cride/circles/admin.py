04cfe338e9a955874f1230f7bc8212761b96dec5
Angel <angel_jadan@outlook.com>
2022-02-02 15:25:29 -0500

    Admin actions and download data rides

--- cride/circles/admin.py
+++ cride/circles/admin.py
@@ -1,10 +1,21 @@
 """Circles admin."""
 
 # Django
+from asyncore import write
 from django.contrib import admin
+
+# Django REST Framework
+from rest_framework import status
 
 # Model
 from cride.circles.models import Circle
+from cride.rides.models import Ride
+from django.http import HttpResponse
+
+# Utilities
+import csv
+from django.utils import timezone
+from datetime import datetime, timedelta
 
 
 @admin.register(Circle)
@@ -25,3 +36,52 @@ class CircleAdmin(admin.ModelAdmin):
         'verified',
         'is_limited'
     )
+
+    actions = ['make_verified', 'make_unverified', 'download_todays_rides']
+
+    def make_verified(self, request, queryset):
+        """Make circle verifyed"""
+        queryset.update(verified=True)
+    make_verified_description = 'Make selected circles verified'
+
+    def make_unverified(self, request, queryset):
+        """Make circle unverifyed"""
+        queryset.update(verified=False)
+    make_unverified_description = 'Make selected circles verified'
+
+    def download_todays_rides(self, request, queryset):
+        """Return today's rides."""
+        now = timezone.now()
+        start = datetime(now.year, now.month, now.day, 0, 0, 0)
+        end = start + timedelta(days=1)
+        rides = Ride.objects.filter(
+            offered_in__in=queryset.values_list(('id')),
+            departure_date__gte=start,
+            departure_date__lte=end
+        ).order_by('departure_date')
+
+        response_http = HttpResponse(content_type='text/csv')
+        response_http['Content-Disposition'] = 'attachment; filename="Rides.csv"'
+        writer = csv.writer(response_http)
+        writer.writerow([
+            'id',
+            'passengers',
+            'departure_location',
+            'departure_date',
+            'arrival_location',
+            'arrival_date',
+            'rating',
+            ])
+        for ride in rides:
+            writer.writerow([
+                ride.pk,
+                ride.passengers.count(),
+                ride.departure_location,
+                str(ride.departure_date),
+                ride.arrival_location,
+                str(ride.arrival_date),
+                ride.rating,
+            ])
+
+        return response_http
+    download_todays_rides_description = 'Download todays rides'
--- cride/circles/tests/test_invitations.py
+++ cride/circles/tests/test_invitations.py
@@ -3,9 +3,14 @@
 # Django
 from django.test import TestCase
 
+# Django REST Framework
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
 # Models
-from cride.circles.models import Circle, Invitation
-from cride.users.models import User
+from cride.circles.models import Circle, Invitation, Membership
+from cride.users.models import User, Profile
 
 
 class InvitationManagerTestCase(TestCase):
@@ -59,3 +64,56 @@ class InvitationManagerTestCase(TestCase):
             code=code
         )
         self.assertNotEqual(code, invitation.code)
+
+
+class MemberInvitationsAPITestCase(APITestCase):
+    """Member invitation API test case."""
+
+    def setUp(self):
+        """Test case setup."""
+        self.user = User.objects.create(
+            first_name='Angel',
+            last_name='Jadan',
+            email='angel_jadan@outlook.com',
+            username='ajadan',
+            password='ajadan1234',
+        )
+        self.profile = Profile.objects.create(user=self.user)
+        self.circle = Circle.objects.create(
+            name='Facultad de Ciencias',
+            slug_name='fciencias',
+            about='Grupo oficial de la Facultad de Ciencias de la UNAM',
+            verify=True
+        )
+        self.membership = Membership.objects.create(
+            user=self.user,
+            profile=self.user.profile,
+            circle=self.circle,
+            remainig_invitations=10
+        )
+        self.token = Token.objects.create(user=self.user).key
+        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(self.token))
+        self.url = '/circles/{}/members/{}/invitations/'.format(
+            self.circle.slug_name,
+            self.user.username
+        )
+
+    def test_response_success(self):
+        """Verify request succed."""
+        request = self.cliente.get(self.url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+    
+    def test_invitation_creation(self):
+        """Verify invitation are generated if none ixist previusly."""
+        # Invitation in DB must be 0
+        self.assertEqual(Invitation.objects.count(), 0)
+
+        # Call member invitations URL
+        request = self.cliente.get(self.url)
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+
+        # Verify new invitations were created
+        invitations = Invitation.objects.filter(issued_by=self.user)
+        self.assertEqual(invitations.count(), self.membership.remainig_invitations)
+        for invitation in invitations:
+            self.assertIn(invitation.code, request.data['invitations'])
